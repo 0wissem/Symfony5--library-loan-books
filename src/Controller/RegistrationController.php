fef9dbc07bbccdc0bbf269cbb467b19be601c2e0
0wissem <wissem.ksentini@gmail.com>
2021-10-28 23:03:42 +0100

    fix roles in  registration form

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -69,33 +69,10 @@ class RegistrationController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
-            return $this->redirectToRoute('home_page');
+            return $this->redirectToRoute('login');
         }catch (\Doctrine\DBAL\Exception $e){
             $error = $e->getPrevious()->getPrevious()->getMessage();
             return $this->render('registration/register.html.twig',['id'=>$error]);
-
         }
-        //$form = $this->createForm(RegistrationFormType::class, $user);
-        // $form->handleRequest($request);
-
-//        if ($form->isSubmitted() && $form->isValid()) {
-//            // encode the plain password
-//            $user->setPassword(
-//                $passwordEncoder->encodePassword(
-//                    $user,
-//                    $form->get('plainPassword')->getData()
-//                )
-//            );
-//
-//            $entityManager = $this->getDoctrine()->getManager();
-//            $entityManager->persist($user);
-//            $entityManager->flush();
-//            // do anything else you need here, like send an email
-//
-//            return $this->redirectToRoute('home_page');
-//        }
-//        return $this->render('registration/register.html.twig', [
-//            // 'registrationForm' => $form->createView(),
-//        ]);
     }
 }
